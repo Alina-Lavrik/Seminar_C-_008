2eec4ed49ca9db36f8ee6016bbe0936f89ad8992
Alina Lavrik <alina_voro@mail.ru>
2022-10-11 21:58:01 +0300

    add task 53_поменять первую и вторую строку массива

---
+++ TASK_001/Program.cs
@@ -0,0 +1,72 @@
+﻿/* **Задача 53:** Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+В итоге получается вот такой массив:
+8 4 2 4
+5 9 2 3
+1 4 7 2 */
+
+
+int[,] array = GetArray(4, 4, 0, 9);
+PrintArray(array);
+Console.WriteLine("");
+
+int[,] Array1 = ChangeArray(array);
+PrintArray(Array1);
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+int[,] ChangeArray(int[,] array)
+{
+    int[,] res = new int[array.GetLength(0), array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == 0)
+            {
+                res[i, j] = array[array.GetLength(0) - 1, j];
+            }
+            else if (i == array.GetLongLength(0) - 1)
+            {
+                res[i, j] = array[0, j];
+            }
+            else
+            {
+                res[i, j] = array[i, j];
+            }
+        }
+        
+    }
+    return res;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
